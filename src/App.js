d1c38117b079888f6ae6b15089c28c4cf0a2b1e6
whezagl <46291976+whezagl@users.noreply.github.com>
2019-01-01 14:34:16 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <div className="row">
+                    <div className="mixed-chart">Dodol</div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
